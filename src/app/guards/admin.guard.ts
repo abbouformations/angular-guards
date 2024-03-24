f7f8d0a2f062c8237299015fdc39b306957c3267
abbouformations <135717843+abbouformations@users.noreply.github.com>
2024-03-24 15:41:59 +0000

    Update admin.guard.ts

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,12 +1,20 @@
-import { CanActivateChildFn } from '@angular/router';
+import { CanActivateChildFn, Router } from '@angular/router';
 import { TokenStorageService } from '../services/token-storage.service';
 
 import { inject } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export const adminGuard: CanActivateChildFn = (childRoute, state) => {
   const tokenStorage = inject(TokenStorageService);
+  const snackBar = inject(MatSnackBar);
+  const router = inject(Router);
+
   if (!tokenStorage.hasRole('ADMIN')) {
-    console.log("vous n'êtes pas autorisé à accéder à cette foncytionnalité");
+    snackBar.open(
+      'You are not allowed to view this page. You are redirected to login Page',
+      'OK'
+    );
+    router.navigate([{ outlets: { primary: 'login', contenu: null } }]);
     return false;
   } else return true;
 };
